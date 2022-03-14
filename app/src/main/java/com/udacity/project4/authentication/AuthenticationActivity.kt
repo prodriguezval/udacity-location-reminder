2879ddbc7f133018e522a8329c4dc95a87ba80e6
Pablo Rodriguez <pablo.rodriguez@thoughtworks.com>
2022-03-14 10:30:11 -0300

    The main activity is now the authentication activity, the firebase auth plugin is configured

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,7 +1,14 @@
 package com.udacity.project4.authentication
 
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
 
 /**
@@ -9,10 +16,21 @@ import com.udacity.project4.R
  * signed in users to the RemindersActivity.
  */
 class AuthenticationActivity : AppCompatActivity() {
+    companion object {
+        const val SIGN_IN_REQUEST_CODE = 1001
+    }
+
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+
 //         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
 
 //          TODO: If the user was authenticated, send him to RemindersActivity
@@ -21,4 +39,33 @@ class AuthenticationActivity : AppCompatActivity() {
         //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
 
     }
+
+    fun onLogin(view: View) {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            // ...
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+        }
+    }
+
 }
