33c57d78e2f474e5a15d17543b10ac1069a860cb
Pablo Rodriguez <pablo.rodriguez@thoughtworks.com>
2022-03-17 10:29:20 -0300

    Login and logout are working properly

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,7 +1,10 @@
 package com.udacity.project4.authentication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -10,16 +13,14 @@ import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.locationreminders.RemindersActivity
 
+private const val TAG = "AuthenticationActivity"
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
  * signed in users to the RemindersActivity.
  */
 class AuthenticationActivity : AppCompatActivity() {
-    companion object {
-        const val SIGN_IN_REQUEST_CODE = 1001
-    }
-
     private val signInLauncher = registerForActivityResult(
         FirebaseAuthUIActivityResultContract()
     ) { res ->
@@ -57,14 +58,12 @@ class AuthenticationActivity : AppCompatActivity() {
     private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
         val response = result.idpResponse
         if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
             val user = FirebaseAuth.getInstance().currentUser
-            // ...
+            Log.i(TAG, "User logged successfully $user")
+            val intent = Intent(this, RemindersActivity::class.java)
+            startActivity(intent)
         } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
+            Toast.makeText(this, R.string.login_failed, Toast.LENGTH_LONG).show()
         }
     }
 
